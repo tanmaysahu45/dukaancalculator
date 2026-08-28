84518ddc6e56ca2ac0a84ee1c5f76a0e2cbd31fe
Tanmay Sahu <tanmaysahu652@gmail.com>
2026-08-28 19:59:25 +0530

    j

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'shop-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    }).catch(() => caches.match('./index.html'))
+  );
+});
